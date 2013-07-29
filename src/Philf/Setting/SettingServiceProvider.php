6c54026f7804384f48d9d3245626d0631301c5f7
PhilF <info@weztec.com>
2013-07-29 17:38:35 +0100

    Indents converted to spaces

--- src/Philf/Setting/SettingServiceProvider.php
+++ src/Philf/Setting/SettingServiceProvider.php
@@ -4,53 +4,53 @@ use Illuminate\Support\ServiceProvider;
 
 class SettingServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('philf/setting');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('philf/setting');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['setting'] = $this->app->share(function($app)
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['setting'] = $this->app->share(function($app)
         {
-			$path     = $this->app['config']->get('setting::setting.path');
-			$filename = $this->app['config']->get('setting::setting.filename');
+            $path     = $this->app['config']->get('setting::setting.path');
+            $filename = $this->app['config']->get('setting::setting.filename');
 
             return new Setting($path, $filename);
         });
 
         $this->app->booting(function()
-		{
-		  $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-		  $loader->alias('Setting', 'Philf\Setting\Facades\Setting');
-		});
-	}
+        {
+          $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+          $loader->alias('Setting', 'Philf\Setting\Facades\Setting');
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('setting');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('setting');
+    }
 
 }
